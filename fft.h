568b0e6a4adc5232d12e31429622f66581f47de4
Mathias Bredholt <mathiasbredholt@gmail.com>
2016-04-15 09:36:32 +0200

    removed adc and fft.

--- fft.h
+++
@@ -1,7 +0,0 @@
-#define ARM_MATH_CM3
-#include "arm_math.h"
-#include "piezo.h"
-
-#define FFT_SIZE 2048
-
-void do_fft(q15_t *input_buffer);
--- robinstr.ino
+++ robinstr.ino
@@ -1,40 +1,20 @@
 #include "Arduino.h"
 #include "pwm_control.h"
-#include "fft.h"
-#include "piezo.h"
-
-#define ARM_MATH_CM3
-#include "arm_math.h"
-
-extern bool PIEZO_INTERRUPT;
-extern volatile int buffer[BUFFER_SIZE];
 
 int main()
 {
     init();
-    Serial.begin(9600);
+    Serial.begin(115200);
 
     Serial.println("Welcome to ROBINSTR v0.1");
 
     init_piezo();
-    init_pwm_controller();
+    // init_pwm_controller();
 
     int now = millis();
 
     while (1)
     {
-        // buffer write finished
-        if (PIEZO_INTERRUPT) {
-            PIEZO_INTERRUPT = false;
 
-            do_fft((q15_t*) buffer);
-            // find_peaks
-            // calc_pid
-        }
-
-        // if ((millis() - now) & 0xFF)
-        // {
-        //     // Serial.println(PIEZO_INTERRUPT);
-        // }
     }
 }
